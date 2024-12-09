8e6acb6cce10e2edd9fbd6716ca00b6c7ee2c8d9
RODOLFO BARRETO DA SILVA <157936293+RodolfoBarreto99@users.noreply.github.com>
2024-12-09 19:12:32 -0300

    atividade 8 UC-05
    correção do arquivo anterior

---
+++ Atividade08_UC05_gamemania/pag_index/pag_login/scripts.js
@@ -0,0 +1,37 @@
+function register() {
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    if (usuario && senha) {
+        localStorage.setItem('usuario', usuario);
+        localStorage.setItem('senha', senha);
+        alert('Usuário cadastrado com sucesso!');
+        window.location.href = 'index.html';
+    } else {
+        alert('Por favor, preencha todos os campos.');
+    }
+}
+
+function login() {
+    const usuario = document.getElementsByName('usuario')[0].value;
+    const senha = document.getElementsByName('senha')[0].value;
+
+    const storedUsuario = localStorage.getItem('usuario');
+    const storedSenha = localStorage.getItem('senha');
+
+    if (usuario === storedUsuario && senha === storedSenha) {
+        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem('currentUser', usuario);
+        alert('Login bem-sucedido!');
+        window.location.href = "../game-mania/index_gm.html"; 
+    } else {
+        alert('Usuário ou senha incorretos.');
+    }
+}
+
+function logout() {
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('currentUser');
+    alert('Logout realizado com sucesso!');
+    window.location.href = "/pag_login/index.html"; 
+}
